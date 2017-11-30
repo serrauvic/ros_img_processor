f6c217375ca18ade9ed8f4f73ca693d5f28a7673
sebastian <sebastian.serra@uvic.cat>
2017-11-30 01:25:53 +0100

    removed dead code

--- src/ros_img_processor_node.cpp
+++ src/ros_img_processor_node.cpp
@@ -6,16 +6,6 @@
 #include "opencv2/core.hpp"
 #include "opencv2/imgproc.hpp"
 
-//constants
-//const int GAUSSIAN_BLUR_SIZE        = 11;
-//const double GAUSSIAN_BLUR_SIGMA    = 2;
-//const double CANNY_EDGE_TH          = 150;
-//const double HOUGH_ACCUM_RESOLUTION = 2;
-//const double MIN_CIRCLE_DIST        = 30;
-//const double HOUGH_ACCUM_TH         = 70;
-//const int MIN_RADIUS                = 20;
-//const int MAX_RADIUS                = 100;
-
 RosImgProcessorNode::RosImgProcessorNode() :
     nh_(ros::this_node::getName()),
     img_tp_(nh_)
@@ -49,66 +39,27 @@ void RosImgProcessorNode::process()
         //copy the input image to the out one
         cv_img_out_.image = cv_img_ptr_in_->image;
 
-        //find the ball
-    		//TODO bucar un cercle
-
-    		//find the direction vector
-    		//TODO amb el cerlce anterior agafem el centre i calculem la d (Ray Direction (in camera frame reference) mirar les diapos
-        //https://campus.uvic.cat/aules/pluginfile.php/360761/mod_resource/content/1/PerceptionSystems_session3.pdf
-        // basicament es aplicar la inversa de la matriu K pel punt del centre.
-        // (per veure la matriu k) rostopic echo /usb_cam/camera_info
-        //
         // vector3.msg.
         // com a punt a part podriem publicar aquesta de en un  missatge com un talker del tutorial 1 que vam fer.
         // publicar un marker per tal que des de rviz es pugui veure la fletxa del vector direcció.
         //
-    std::vector<cv::Vec3f> circles;
-    cv::Point center;
-    //cv::Mat gray_image;
-    int radius;
+        std::vector<cv::Vec3f> circles;
 
+        Hough_Transform::calculate(cv_img_out_.image, circles);
 
-    //clear previous circles
-    //circles.clear();
-    // If input image is RGB, convert it to gray
-    //cv::cvtColor(cv_img_out_.image, gray_image, CV_BGR2GRAY);
-    //Reduce the noise so we avoid false circle detection
-    //cv::GaussianBlur( gray_image, gray_image, cv::Size(GAUSSIAN_BLUR_SIZE, GAUSSIAN_BLUR_SIZE), GAUSSIAN_BLUR_SIGMA );
-    //Apply the Hough Transform to find the circles
-    //cv::HoughCircles( gray_image, circles, CV_HOUGH_GRADIENT, HOUGH_ACCUM_RESOLUTION, MIN_CIRCLE_DIST, CANNY_EDGE_TH, HOUGH_ACCUM_TH, MIN_RADIUS, MAX_RADIUS );
-
-
-    Hough_Transform::calculate(cv_img_out_.image, circles);
-
-    //draw circles on the image
-    for(unsigned int ii = 0; ii < circles.size(); ii++ )
-    {
-        if ( circles[ii][0] != -1 )
+        //draw circles on the image
+        for(unsigned int ii = 0; ii < circles.size(); ii++ )
         {
+            if ( circles[ii][0] != -1 )
+            {
+
+                cv::Point center;
+                int radius;
                 Img_Circle::get_center_coordinates(circles[ii], center, radius);
-                //center = cv::Point(cvRound(circles[ii][0]), cvRound(circles[ii][1]));
-                //radius = cvRound(circles[ii][2]);
-                //cv::circle(cv_img_out_.image, center, 5, cv::Scalar(128, 255, 0), -1, 8, 0 );// circle center in green
-                //cv::circle(cv_img_out_.image, center, radius, cv::Scalar(0, 0, 255), 3, 8, 0 );// circle perimeter in red
-
                 draw_clircle(center, radius, true/*draw circle center coordinates*/);
-
-                // circle center point x and y coordinates.
-                //std::ostringstream stringStream;
-                //stringStream  << "  x:" << center.x << "\n" << "y:" << center.y;
-                // print circle center coordinates
-                //cv::putText(cv_img_out_.image, stringStream.str(), center, cv::FONT_HERSHEY_PLAIN, 0.8, cv::Scalar(0, 255, 0), 2, 0.5);
-
                 Img_Circle::get_ray_direction(matrixK_, center, ray_direction_);
-
-                //cv::Mat kinverted;
-                //cv::invert(matrixK_, kinverted, cv::DECOMP_LU) ;
-
-                //ray_direction = (cv::Mat_<double>(3,1) << 0, 0, 0) ;
-                //cv::Mat centerPoint = (cv::Mat_<double>(3,1) << center.x, center.y, 1.0);
-				        //ray_direction = kinverted * centerPoint;
+            }
         }
-    }
 
 
         //sets and draw a bounding box around the ball
